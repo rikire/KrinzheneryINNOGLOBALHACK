4c587c597c4d9421f0e159219e70c38440ab11df
rikire <56105683+rikire@users.noreply.github.com>
2024-11-03 19:30:08 +0300

    added cors

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 import app.api.endpoints as endpoints
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Подключаем роуты
 app.include_router(endpoints.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change to specific domains in production
+    allow_credentials=True,
+    allow_methods=["*"],  # Change to specific methods as needed
+    allow_headers=["*"],  # Change to specific headers as needed
+)
